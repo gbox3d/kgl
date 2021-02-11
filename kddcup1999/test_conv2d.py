ed8b5aa5fa51f04df120de2fb23f35501dae96db
gbox3d <gbox3d@gmail.com>
2021-02-11 23:39:50 +0900

    ...

--- kddcup1999/test_conv2d.py
+++ kddcup1999/test_conv2d.py
@@ -64,7 +64,7 @@ from sklearn.metrics import accuracy_score
 
 #%%
 
-_x_test = np.reshape(x_test,(163027,5,6))
+_x_test = np.reshape(x_test,(x_test.shape[0],5,6))
 _x_test = np.expand_dims(_x_test,axis=3)
 
 #%%
